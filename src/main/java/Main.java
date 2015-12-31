e208a5f6f4ca9957e97ddce05bb6a817ae4635ce
dinamix <tristano.tenaglia@gmail.com>
2015-12-30 21:18:59 -0500

    properties added but file name requires system property access which cannot be done through java

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -6,12 +6,15 @@ import ttecca.client_java_library.dao.MetricLogger;
 public class Main {
 
 	public static void main(String[] args) {
-		MetricLogger mw = new MetricLogger("app_test", "host_test", "market_test");
-		mw.writeMetric("blowme", "blowmemore", 69);
-		File d = new File("/tmp");
+		//TODO this needs to be set here and cannot be set in java code due to permissions
+		//TODO try this on linux system, windows might just be mucking it up
+		System.setProperty("ttecca.logfile.name", "/tmp/service_log");
+		MetricLogger mw = new MetricLogger("/tmp","app_test", "host_test", "market_test");
+		mw.writeMetric("op_test", "metric_test", 12);
+		/*File d = new File("/tmp");
 		for(File f : d.listFiles()) {
 			System.out.println(f);
-		}
+		}*/
 	}
 
 }
--- src/main/java/ttecca/client_java_library/dao/MetricLogger.java
+++ src/main/java/ttecca/client_java_library/dao/MetricLogger.java
@@ -11,20 +11,23 @@ import lombok.AllArgsConstructor;
 import lombok.extern.log4j.Log4j;
 
 @Log4j
-@AllArgsConstructor
 public class MetricLogger {
+	//TODO could check this property when running things from cmd line
+	//http://stackoverflow.com/questions/1324053/configure-log4j-to-log-to-custom-file-at-runtime
+	private final String logDirectory;
 	private final String applicationName;
 	private final String hostName;
 	private final String marketPlace;
 	
 	private final ObjectMapper mapper = new ObjectMapper();
 	
-	//This could also be specified by the constructor
-	//If we want a user specified log output
-	static {
-		//This line is required in order to set the service_log output folder
-		//TODO Should add tmp folder checking here
-		System.setProperty("logfile.name", "/tmp/service_log");
+	public MetricLogger(String logDirectory,String applicationName,String hostName,String marketPlace) {
+		this.logDirectory = logDirectory;
+		this.applicationName = applicationName;
+		this.hostName = hostName;
+		this.marketPlace = marketPlace;
+		//TODO currently does not work on windows
+		//System.setProperty("ttecca.logfile.name", "/tmp/service_log");
 	}
 	
 	public void writeMetric(String operationName, String metricName, double metricValue) {
