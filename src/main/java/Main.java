2e80b11fdf02ceaa91b3cb8658c5510a4fb49c84
dinamix <tristano.tenaglia@mail.mcgill.ca>
2016-01-04 23:22:54 -0500

    updated client library with static calls and log4j

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -8,15 +8,17 @@ public class Main {
 	public static void main(String[] args) throws InterruptedException {
 		//TODO this needs to be set here and cannot be set in java code due to permissions
 		//TODO try this on linux system, windows might just be mucking it up
-		/*System.setProperty("ttecca.logfile.name", "/tmp/service_log");
-		MetricLogger mw = new MetricLogger("/tmp","app_test", "host_test", "market_test");
+		System.setProperty("ttecca.logfile.name", "/tmp/service/service_log");
+		
+		//Need to call this before using to instantiate
+		MetricLogger.build("/tmp/service","app_test", "host_test", "market_test");
 		
 		long startTime = System.currentTimeMillis();
 		while((startTime - System.currentTimeMillis())<310*1000) {
 			double random = Math.random() * 1000;
-			mw.writeMetric("op_test", "metric_test", random);
-			Thread.sleep(20000);
-		}*/
+			MetricLogger.writeMetric("op_test", "nicishard", random);
+			Thread.sleep(10000);
+		}
 		File d = new File("/tmp");
 		for(File f : d.listFiles()) {
 			System.out.println(f);
--- src/main/java/ttecca/client_java_library/dao/MetricLogger.java
+++ src/main/java/ttecca/client_java_library/dao/MetricLogger.java
@@ -8,31 +8,41 @@ import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.ObjectMapper;
 
 import lombok.AllArgsConstructor;
+import lombok.Getter;
 import lombok.extern.log4j.Log4j;
 
 @Log4j
 public class MetricLogger {
+	//Singleton metric logger
+	private static MetricLoggerInit logger;
+	
 	//TODO could check this property when running things from cmd line
 	//http://stackoverflow.com/questions/1324053/configure-log4j-to-log-to-custom-file-at-runtime
-	private final String logDirectory;
-	private final String applicationName;
-	private final String hostName;
-	private final String marketPlace;
-	
-	private final ObjectMapper mapper = new ObjectMapper();
-	
-	public MetricLogger(String logDirectory,String applicationName,String hostName,String marketPlace) {
-		this.logDirectory = logDirectory;
-		this.applicationName = applicationName;
-		this.hostName = hostName;
-		this.marketPlace = marketPlace;
-		//TODO currently does not work on windows
-		//System.setProperty("ttecca.logfile.name", "/tmp/service_log");
+	@AllArgsConstructor
+	@Getter
+	private static class MetricLoggerInit {
+		private final String logDirectory;
+		private final String applicationName;
+		private final String hostName;
+		private final String marketPlace;
 	}
 	
-	public void writeMetric(String operationName, String metricName, double metricValue) {
+	private final static ObjectMapper mapper = new ObjectMapper();
+	
+	public static void build(String logDirectory, String applicationName, String hostName, String marketPlace) {
+		if(logger == null) {
+			logger = new MetricLoggerInit(logDirectory,applicationName,hostName,marketPlace);
+		}
+	}
+
+	public static void writeMetric(String operationName, String metricName, double metricValue) {
+		if(logger == null) {
+			throw new IllegalAccessError("Need to initialize/build MetricLogger before writing metrics");
+		}
 		long currentTime = DateTime.now().getMillis();
-		StagedMetric metric = new StagedMetric(applicationName,operationName,marketPlace,hostName,currentTime,metricName,metricValue);
+		StagedMetric metric = new StagedMetric(logger.getApplicationName(),
+					operationName,logger.getMarketPlace(),logger.getHostName(),
+					currentTime,metricName,metricValue);
 		try {
 			String jsonString = mapper.writeValueAsString(metric);
 			log.info(jsonString);
